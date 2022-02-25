c955b0d418c63373efb3f1399b3076a749675981
Dang Van Trung <21115053120156@sv.ute.udn.vn>
2022-02-25 10:52:15 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Alert
+alert(`Tip hay khi đi đường:
+Tui có người yêu rùi, anh tránh ra đi, tui la lên á!!!`);
+
+// Prompt
+const nyName = prompt("Hãy cho biết họ tên người yêu của bạn: ");
+alert(`Nguyễn Ngọc Như Yến đang hẹn hò với ${nyName} nè <3`);
+
+// Confirm
+const isYourHusban = confirm("Trung có phải là chồng của bạn hong?");
+if (isYourHusban) {
+  alert("Trả lời đúng gòi, yêu yêu <3 <3");
+} else {
+  alert("Trời ơi, hư nha, huhu khókkkkkk :(");
+}
